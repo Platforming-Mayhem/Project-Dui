79e7dfe2bcc724948f7860e8cbaf62a155f1e9e4
Platforming-Mayhem <kajawad03@gmail.com>
2021-11-16 15:53:03 +0000

    -Added scuffed Resident Evil 2 Camera

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,20 +4,59 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    public GameObject Player;
+    public GameObject player;
     public float speed;
+    public Vector3 scale;
+    public Vector3 offset;
     Vector3 initialPosition;
+    Camera cam;
+    bool check;
+
     // Start is called before the first frame update
     void Start()
     {
         initialPosition = transform.position;
         Cursor.lockState = CursorLockMode.Locked;
+        cam = GetComponentInChildren<Camera>();
+    }
+
+    Vector3 maxExt;
+    Vector3 minExt;
+
+    private void FixedUpdate()
+    {
+        maxExt = (transform.position + offset + (scale / 2f));
+        minExt = (transform.position + offset - (scale / 2f));
+        if(player.transform.position.x < maxExt.x && player.transform.position.x > minExt.x && player.transform.position.z < maxExt.z && player.transform.position.z > minExt.z && player.transform.position.y < maxExt.y && player.transform.position.y > minExt.y)
+        {
+            check = true;
+        }
+        else
+        {
+            check = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, Player.transform.position + initialPosition, Time.deltaTime * speed);
-        transform.eulerAngles += Vector3.up * Time.deltaTime * Input.GetAxis("Mouse X") * speed;
+
+        
+
+        if (check)
+        {
+            cam.gameObject.SetActive(true);
+            Debug.Log("Colliding, Your chin feels nice");
+            transform.LookAt(player.transform);
+        }
+        else
+        {
+            cam.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawCube(transform.position + offset, scale);
     }
 }
