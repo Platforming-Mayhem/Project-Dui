5e5790b214d12d492d3d1fe099b3962fb24deae7
Platforming-Mayhem <kajawad03@gmail.com>
2021-11-17 10:37:42 +0000

    Can rotate Camera changers Still need to fix the thing where you can walk in and out of a camera changer and it still changes the camera no matter what side you are on.

--- Assets/Scripts/ChangeCamera.cs
+++ Assets/Scripts/ChangeCamera.cs
@@ -6,26 +6,31 @@ public class ChangeCamera : MonoBehaviour
 {
     GameObject player;
     CameraScript cam;
+    public bool changeCamera;
     public Vector3 offset;
     public Vector3 size;
     bool check;
     bool once;
-    Vector3 maxExt;
-    Vector3 minExt;
+    public Vector3 maxExt;
+    public Vector3 minExt;
+    Vector3 nMaxExt;
+    Vector3 nMinExt;
     Vector3 initalPosition;
+    Vector3 center;
     // Start is called before the first frame update
     void Start()
     {
+        center = transform.position + offset;
         player = GameObject.FindGameObjectWithTag("Player");
         cam = FindObjectOfType<CameraScript>();
-        maxExt = transform.position + offset + (size / 2f);
-        minExt = transform.position + offset - (size / 2f);
         initalPosition = cam.position;
+        nMinExt = transform.rotation * minExt;
+        nMaxExt = transform.rotation * maxExt;
     }
 
     private void FixedUpdate()
     {
-        if (player.transform.position.x < maxExt.x && player.transform.position.x > minExt.x && player.transform.position.y < maxExt.y && player.transform.position.y > minExt.y && player.transform.position.z < maxExt.z && player.transform.position.z > minExt.z)
+        if (player.transform.position.x < nMaxExt.x && player.transform.position.x > nMinExt.x && player.transform.position.y < nMaxExt.y && player.transform.position.y > nMinExt.y && player.transform.position.z < nMaxExt.z && player.transform.position.z > nMinExt.z)
         {
             check = true;
         }
@@ -42,15 +47,18 @@ public class ChangeCamera : MonoBehaviour
     {
         if(!check && previousCheck)
         {
-            if(cam.cameraTypes == CameraScript.CameraTypes.dolly)
+            cam.position = transform.position;
+            if (!changeCamera)
             {
-                cam.position = initalPosition;
-                cam.cameraTypes = CameraScript.CameraTypes.mouseControl;
-            }
-            else
-            {
-                cam.position = transform.position;
-                cam.cameraTypes = CameraScript.CameraTypes.dolly;
+                if (cam.cameraTypes == CameraScript.CameraTypes.dolly)
+                {
+                    cam.position = initalPosition;
+                    cam.cameraTypes = CameraScript.CameraTypes.mouseControl;
+                }
+                else
+                {
+                    cam.cameraTypes = CameraScript.CameraTypes.dolly;
+                }
             }
         }
         previousCheck = check;
@@ -58,6 +66,18 @@ public class ChangeCamera : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireCube(transform.position + offset, size);
+        Gizmos.DrawLine(transform.rotation * minExt, transform.rotation * maxExt);
+        Gizmos.DrawLine(transform.rotation * minExt, transform.rotation * new Vector3(maxExt.x, minExt.y, minExt.z));
+        Gizmos.DrawLine(transform.rotation * new Vector3(minExt.x, maxExt.y, minExt.z), transform.rotation * new Vector3(maxExt.x, maxExt.y, minExt.z));
+        Gizmos.DrawLine(transform.rotation * new Vector3(minExt.x, minExt.y, minExt.z), transform.rotation * new Vector3(minExt.x, maxExt.y, minExt.z));
+        Gizmos.DrawLine(transform.rotation * new Vector3(maxExt.x, minExt.y, minExt.z), transform.rotation * new Vector3(maxExt.x, maxExt.y, minExt.z));
+        Gizmos.DrawLine(transform.rotation * new Vector3(minExt.x, minExt.y, maxExt.z), transform.rotation * new Vector3(maxExt.x, minExt.y, maxExt.z));
+        Gizmos.DrawLine(transform.rotation * new Vector3(minExt.x, maxExt.y, maxExt.z), transform.rotation * new Vector3(maxExt.x, maxExt.y, maxExt.z));
+        Gizmos.DrawLine(transform.rotation * new Vector3(minExt.x, minExt.y, maxExt.z), transform.rotation * new Vector3(minExt.x, maxExt.y, maxExt.z));
+        Gizmos.DrawLine(transform.rotation * new Vector3(maxExt.x, minExt.y, maxExt.z), transform.rotation * new Vector3(maxExt.x, maxExt.y, maxExt.z));
+        Gizmos.DrawLine(transform.rotation * new Vector3(minExt.x, minExt.y, minExt.z), transform.rotation * new Vector3(minExt.x, minExt.y, maxExt.z));
+        Gizmos.DrawLine(transform.rotation * new Vector3(minExt.x, maxExt.y, minExt.z), transform.rotation * new Vector3(minExt.x, maxExt.y, maxExt.z));
+        Gizmos.DrawLine(transform.rotation * new Vector3(maxExt.x, minExt.y, minExt.z), transform.rotation * new Vector3(maxExt.x, minExt.y, maxExt.z));
+        Gizmos.DrawLine(transform.rotation * new Vector3(maxExt.x, maxExt.y, minExt.z), transform.rotation * new Vector3(maxExt.x, maxExt.y, maxExt.z));
     }
 }
